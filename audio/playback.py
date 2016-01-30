b635f6bd94167fc1d2ca6e6404f33bdbdab2628c
danieleilbott <danny.eilbott@gmail.com>
2016-01-30 10:36:05 +0000

    Moved playback to /audio

---
+++ audio/playback.py
@@ -0,0 +1,66 @@
+from array import array
+import pyaudio
+import wave
+import pygame
+
+ 
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 48000
+CHUNK = 1024
+RECORD_SECONDS = 5
+WAVE_OUTPUT_FILENAME = "file.wav"
+ 
+audio = pyaudio.PyAudio()
+ 
+# start Recording
+stream = audio.open(format=FORMAT, channels=CHANNELS,
+                rate=RATE, input=True,
+                frames_per_buffer=CHUNK)
+print("recording...")
+frames = []
+ 
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    as_ints = array('h', data)
+    max_value = max(as_ints)
+    print(max_value)
+    frames.append(data)
+print("finished recording")
+ 
+ 
+# stop Recording
+stream.stop_stream()
+stream.close()
+audio.terminate()
+ 
+waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+waveFile.setnchannels(CHANNELS)
+waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+waveFile.setframerate(RATE)
+waveFile.writeframes(b''.join(frames))
+waveFile.close()
+
+'''
+CHANNELS = 1
+swidth = 2
+Change_RATE = 3.5
+
+spf = wave.open('out.wav', 'rb')
+RATE=spf.getframerate()
+signal = spf.readframes(-1)
+
+wf = wave.open('out_adjusted.wav', 'wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(swidth)
+wf.setframerate(RATE*Change_RATE)
+wf.writeframes(signal)
+wf.close()
+'''
+
+pygame.mixer.init(48000)
+pygame.mixer.music.load("file.wav")
+pygame.sndarray.array(pygame.mixer.music)
+pygame.mixer.music.play()
+while pygame.mixer.music.get_busy() == True:
+    continue
